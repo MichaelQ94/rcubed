80e27eb1c2c9e8a0eba10d4f939b37e841e09881
Michael Qian <michaelq94@gmail.com>
2020-02-19 23:01:30 -0800

    Replaces array-based index map implementation of cube operations with switches. More memory-efficient and looks cleaner.

--- mbq/rcubed/cube3/core/ElemCube3Op.java
+++ mbq/rcubed/cube3/core/ElemCube3Op.java
@@ -2,23 +2,59 @@ package mbq.rcubed.cube3.core;
 
 /** Represents elementary cube operations in the form of clockwise face rotations. */
 public enum ElemCube3Op implements Cube3Op {
-  R(ElemFaceletMaps.R),
-  R2(ElemFaceletMaps.R2),
-  R3(ElemFaceletMaps.R3);
+  R {
+    @Override
+    public int imageOf(int index) {
+      switch(index) {
+        // R
+        case 27: return 29;
+        case 28: return 32;
+        case 29: return 35;
+        case 32: return 34;
+        case 35: return 33;
+        case 34: return 30;
+        case 33: return 27;
+        case 30: return 28;
 
-  private final int[] faceletMap;
+        // U -> B
+        case 2: return 42;
+        case 5: return 39;
+        case 8: return 36;
 
-  private ElemCube3Op(int[] faceletMap) {
-    this.faceletMap = faceletMap;
-  }
+        // F -> U
+        case 20: return 2;
+        case 23: return 5;
+        case 26: return 8;
+
+        // D -> F
+        case 47: return 20;
+        case 50: return 23;
+        case 53: return 26;
+
+        // B -> D
+        case 36: return 53;
+        case 39: return 50;
+        case 42: return 47;
+
+        default: return index;
+      }
+    }
+  },
+  R2 {
+    @Override
+    public int imageOf(int index) {
+      return R.imageOf(R.imageOf(index));
+    }
+  },
+  R3 {
+    @Override
+    public int imageOf(int index) {
+      return R.imageOf(R.imageOf(R.imageOf(index)));
+    }
+  };
 
   @Override
-  public int imageOf(int index) {
-    return faceletMap[index];
-  }
-
-  @Override
-  public Cube3Op inverse() {
+  public ElemCube3Op inverse() {
     return inverseOf(this);
   }
 
