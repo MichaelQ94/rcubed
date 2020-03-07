4895ffd4bb5d8ec2cd29541706c94cf8400e8c52
Michael Qian <michaelq94@gmail.com>
2020-03-06 17:05:53 -0800

    Basic cube representation types

---
+++ src/lib/core/cube.tsx
@@ -0,0 +1,9 @@
+import { CubeIndex } from "./cubeindex"
+import { CubeOp } from "./cubeop"
+
+export interface Cube {
+  readonly dimension: number;
+  readonly positionOf: (facelet: number) => CubeIndex;
+  readonly occupantOf: (position: CubeIndex) => number;
+  readonly apply: (op: CubeOp) => Cube;
+}
