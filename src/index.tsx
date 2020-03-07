390a3c25841a5945536cb7fb28c2d686527e5601
Richard Tang <richrdtang@gmail.com>
2020-03-06 20:46:40 -0800

    Adding linting to Webpack and configuring absolute import pathing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-import App from "./components/app";
+import App from "components/app";
 
 window.addEventListener("DOMContentLoaded", () => {
   console.log("JavaScript is Working");
